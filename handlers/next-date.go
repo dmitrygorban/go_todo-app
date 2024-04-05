a577744e364323136dbdb4cce90f5c86f584fc6c
d.gorban <dg@itm.tech>
2024-04-05 11:50:03 +0200

    add next date handling for years and days

---
+++ handlers/next-date.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	utils "github.com/dmitrygorban/go_todo-app"
+)
+
+func GetNextDate(w http.ResponseWriter, r *http.Request) {
+	now := r.FormValue("now")
+	date := r.FormValue("date")
+	repeat := r.FormValue("repeat")
+	nowTime, err := time.Parse("20060102", now)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Error %s", err)
+		return
+	}
+	next, err := utils.NextDate(nowTime, date, repeat)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Error %s", err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(next))
+}
